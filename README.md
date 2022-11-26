ea6f5d8cbf4136258d91858b940c452aeca4a907
Roman Munteanu <roma.mun@gmail.com>
2022-11-26 11:35:03 +0200

    Implement OTP verification

--- README.md
+++ README.md
@@ -12,40 +12,66 @@ go mod vendor
 go run .\main.go
 ```
 
+## OTP Verification
+
+Enable `app.verify()` and specify `phoneNumberTo` then send vetification code:
+```
+go run .\main.go
+```
+
+Comment `app.verify()` and enable `app.checkVerificationToken()` and provide the code your received in SMS:
+```
+go run .\main.go -t 065729
+
+token: 065729
+approved
+```
+
 ## Resources
 
 * Go quickstart example:
+
 https://www.twilio.com/docs/sms/quickstart/go
 
 * Webhooks:
+
 https://www.twilio.com/docs/usage/webhooks
 
 SMS Webhooks (to check if the message has been delivered successfully):
+
 https://www.twilio.com/docs/usage/webhooks/sms-webhooks
 
 user-defined HTTP callback (webhook) example:
+
 https://www.twilio.com/docs/sms/tutorials/how-to-confirm-delivery-java
 
 
 * Phone number validation:
+
 https://www.twilio.com/docs/lookup/api
 
 Validate a national phone number
+
 https://www.twilio.com/docs/lookup/tutorials/validation-and-formatting#
 
 * E.164 format
+
 https://support.twilio.com/hc/en-us/articles/223183008-Formatting-International-Phone-Numbers
 
 * The list of country calling codes:
+
 https://en.wikipedia.org/wiki/List_of_country_calling_codes#Alphabetical_listing_by_country_or_region
 
 * Using a list of allowed country codes:
+
 https://www.twilio.com/blog/allow-list-country-code-lookup
 
 * Best practices for phone number validation during new user enrollment
+
 https://www.twilio.com/blog/best-practices-phone-number-validation-user-enrollment
 
 * Trial limitations
+
 https://www.twilio.com/docs/usage/tutorials/how-to-use-your-free-trial-account#trial-account-restrictions-and-limitations
 
 * Scaling with Messaging Services 
@@ -53,4 +79,8 @@ https://www.twilio.com/docs/usage/tutorials/how-to-use-your-free-trial-account#t
 https://www.twilio.com/docs/messaging/services
 
 https://www.twilio.com/docs/messaging/guides/best-practices-at-scale
+
+* Verify API
+
+https://www.twilio.com/docs/verify/api#
 
--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/twilio/twilio-go"
 	api "github.com/twilio/twilio-go/rest/api/v2010"
+	verify "github.com/twilio/twilio-go/rest/verify/v2"
 )
 
 type TwilioAdapterApp struct {
-	ctx    context.Context
-	client *twilio.RestClient
+	ctx        context.Context
+	client     *twilio.RestClient
+	serviceSID string
 }
 
 /*
@@ -24,6 +28,22 @@ func main() {
 		client: twilio.NewRestClient(),
 	}
 
+	serviceSID := os.Getenv("TWILIO_VERIFY_SERVICE_SID")
+	if serviceSID == "" {
+		fmt.Println("Twilio Verification Service SID not found")
+		return
+	}
+
+	app.serviceSID = serviceSID
+
+	// app.sendMessage()
+
+	// app.verify()
+
+	app.checkVerificationToken()
+}
+
+func (app *TwilioAdapterApp) sendMessage() {
 	phoneNumberFrom := "+..." // phone number bought on Twilio (Buy a number)
 	phoneNumberTo := "+..."   // Vetified Caller ID (my local phone number)
 
@@ -44,5 +64,57 @@ func main() {
 		fmt.Println(*resp.Sid)
 	} else {
 		fmt.Println(resp.Sid)
+	}
+}
+
+func (app *TwilioAdapterApp) verify() {
+	phoneNumberTo := "+..." // my local phone number
+
+	params := &verify.CreateVerificationParams{}
+	params.SetTo(phoneNumberTo)
+	params.SetChannel("sms")
+
+	resp, err := app.client.VerifyV2.CreateVerification(app.serviceSID, params)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("%+v\n", resp)
+
+	if resp.Status != nil {
+		fmt.Println(*resp.Status)
+	} else {
+		fmt.Println(resp.Status)
+	}
+}
+
+func (app *TwilioAdapterApp) checkVerificationToken() {
+	var token string
+	flag.StringVar(&token, "t", "", "enter verification code")
+	flag.Parse()
+
+	if token == "" {
+		fmt.Println("please specify the verification code")
+		return
+	}
+	fmt.Println("token:", token)
+
+	phoneNumberTo := "+..." // my local phone number
+
+	params := &verify.CreateVerificationCheckParams{}
+	params.SetTo(phoneNumberTo)
+	params.SetCode(token)
+
+	resp, err := app.client.VerifyV2.CreateVerificationCheck(app.serviceSID, params)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if resp.Status != nil {
+		fmt.Println(*resp.Status)
+	} else {
+		fmt.Println(resp.Status)
 	}
 }
